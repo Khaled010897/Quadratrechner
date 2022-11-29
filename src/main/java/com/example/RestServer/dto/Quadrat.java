34a719af7850dadf8d1fdb4eaadce1e5ccfd23c1
Khaled Daoud <ic21b019@technikum-wien.at>
2022-11-29 19:08:32 +0100

    Test

--- src/main/java/com/example/RestServer/dto/Quadrat.java
+++ src/main/java/com/example/RestServer/dto/Quadrat.java
@@ -3,7 +3,7 @@ package com.example.RestServer.dto;
 public class Quadrat {
     private long ergebnis = 0;
 
-    public long getErgebnis() {return ergebnis;
+    public long getErgebnis() {return ergebnis;//Kommentar zum testen
     }
 
     public void quadratberechnen(long zahl) {ergebnis = zahl*zahl;}
